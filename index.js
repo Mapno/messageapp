b7199e93825a8294996c74f91fa481f358ea2e38
Mapno <mariorvinas@gmail.com>
2018-10-29 16:01:24 +0100

    redirect messages to messageapp and receive response

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const axios = require("axios");
+const bodyParser = require("body-parser");
 
-app.listen(9001, () => console.log('Hola, mundo'));
+const appRedirect = axios.create({
+    baseURL: "http://localhost:3000"
+});
 
-// app.post('/message', (req, res, next) => {
-//     const { destination, message } = req.body;
-//     res.send(message, destination);
-// });
+app.use(bodyParser.json());
+
+app.listen(9001, () => console.log("Hola, mundo"));
+
+app.post("/message", (req, res, next) => {
+    const { destination, body } = req.body;
+    appRedirect.post("/message", { destination, body })
+        .then(response => res.send(response.data))
+        .catch(error => res.send(`Error: ${error}`));
+});
+
+app.post('/test', (req, res, next) => {
+    console.log('funciona que no es poco')
+    res.send({'esto': 'va bien'})
+})
