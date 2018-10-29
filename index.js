695e647d2279d01a9a0157a2f9a05ff96c56b58b
Mapno <mariorvinas@gmail.com>
2018-10-29 14:38:23 +0100

    fixed identation docker-compose

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.listen(9001, () => console.log('Hola, mundo'))
+app.listen(9001, () => console.log('Hola, mundo'));
+
+// app.post('/message', (req, res, next) => {
+//     const { destination, message } = req.body;
+//     res.send(message, destination);
+// });
