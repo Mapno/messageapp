298d564bbbad99b98055c0df5a2a07440f85bb01
Mapno <mariorvinas@gmail.com>
2018-10-29 16:14:29 +0100

    error handling

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const axios = require("axios");
 const bodyParser = require("body-parser");
+
+const port = process.env.PORT
 
 const appRedirect = axios.create({
     baseURL: "http://localhost:3000"
@@ -9,16 +12,11 @@ const appRedirect = axios.create({
 
 app.use(bodyParser.json());
 
-app.listen(9001, () => console.log("Hola, mundo"));
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.post("/message", (req, res, next) => {
     const { destination, body } = req.body;
     appRedirect.post("/message", { destination, body })
-        .then(response => res.send(response.data))
-        .catch(error => res.send(`Error: ${error}`));
+        .then(response => res.status(200).send(response.data))
+        .catch(error => res.status(500).send(`Error: comunication failed`));
 });
-
-app.post('/test', (req, res, next) => {
-    console.log('funciona que no es poco')
-    res.send({'esto': 'va bien'})
-})
