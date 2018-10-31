25084232d1b10018bbd88665736bcb02172e54f8
Mapno <mariorvinas@gmail.com>
2018-10-31 18:36:20 +0100

    added tests for the client

--- client-messageapp/test.js
+++
@@ -1,5 +0,0 @@
-const Client = require('./client')
-const client = new Client('localhost', 9001)
-
-client.send('cabify', 'prueba')
-    .then(res => console.log(res))
--- service/index.js
+++ service/index.js
@@ -5,10 +5,11 @@ const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 9001;
 
-
 app.use(bodyParser.json());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 const messageRouter = require('./message/message');
 app.use('/message', messageRouter);
+
+module.exports = app;
