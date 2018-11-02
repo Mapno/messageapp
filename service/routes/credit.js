b2e541ea5fb98ba50b382f85b7b1fbbbfc61b81b
Mapno <mariorvinas@gmail.com>
2018-11-02 17:44:22 +0100

    fixed /credit increment

--- service/routes/credit.js
+++ service/routes/credit.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const Credit = require('../models/Credit');
 const { creditValidator } = require('../utils/validationMiddleware');
 
-router.post('/', creditValidator, (req,res,next) => {
+router.post('/', creditValidator, (req, res, next) => {
+
     const { amount } = req.body;
-    Credit.findOneAndUpdate({},{ amount }, {new: true})
+
+    Credit.findOneAndUpdate({}, { $inc: { "amount": amount } }, { new: true })
         .then(credit => res.status(200).json(credit))
         .catch(err => res.status(500).send('Error', err))
 })
