06e41dd1c418dbda8209d3e555cb0e2a1a1b8f79
Mapno <mariorvinas@gmail.com>
2018-11-03 17:07:03 +0100

    added rate limiter

--- service/index.js
+++ service/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const rateLimit = require('./utils/limiter');
 
 const port = process.env.PORT || 9001;
 
@@ -9,6 +10,8 @@ const dbURL = 'mongodb://localhost:27017/messageapp';
 const { connect } = require('./database/DatabaseService');
 connect(dbURL);
 
+app.use(rateLimit);
+app.use(bodyParser.urlencoded({ limit: '1mb', extended: false }));
 app.use(bodyParser.json({ limit: '1mb' }));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
